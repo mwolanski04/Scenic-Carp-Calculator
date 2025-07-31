4acb9070919957bcfc10a924e9ff4743fee46b0c
Matthew Wolanski <matthew.wolanski@slu.edu>
2025-07-30 21:49:17 -0500

    ft and in selector

--- src/App.js
+++ src/App.js
@@ -5,56 +5,172 @@ import Button from "./components/Button";
 import React, { useState } from "react";
 
 const btnValues = [
-  ["C", "+-", "%", "/"],
-  [7, 8, 9, "X"],
-  [4, 5, 6, "-"],
-  [1, 2, 3, "+"],
-  [0, ".", "="],
+  ["C", "+-", "%", "/", "ft/in"],
+  [7, 8, 9, "X", "1/2"],
+  [4, 5, 6, "-", "1/4"],
+  [1, 2, 3, "+", "1/8"],
+  [0, ".", "=", "1/16" ],
 ];
 
 
 const App = () => {
 
   let [calc, setCalc] = useState({
-    sign: "+",
+    isFt: true,
+    sign: "",
     num: 0,
     res: 0,
   });
+
+  let [fraction, setFraction] = useState({
+    num: 0,
+    res: 0,
+  });
+
+  const clearCalc = () => {
+    setCalc({sign: "+", num: 0, res: 0, isFt: calc.isFt});
+    setFraction({num: 0, res:0})
+
+  }
+
+  const signClick = (btn) => {
+    setCalc({
+      sign: btn,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num:0,
+    })
+  }
+
+  const equalsClick = () => {
+    var out = 0
+    if (calc.sign && calc.num) {
+      var a = Number(calc.res)
+      var b = Number(calc.num)
+      calc.sign === "+"
+      ? out = a + b
+      : calc.sign === "-"
+      ? out = a - b
+      : calc.sign === "x"
+      ? out = a * b
+      : out = a / b
+    }
+    setCalc({
+      res: out,
+      sign: "",
+      num:0,
+    })
+
+  }
+
+  const percentClick = () => {
+      setCalc({
+        num: calc.num / 100
+      })
+    }
+
+    const changeSign = () => {
+      setCalc({
+        num: calc.num != 0
+        ? calc.num * - 1
+        : 0,
+        res: calc.res
+
+      })
+    }
+
+    const decimalClick = () => {
+      setCalc({
+        num: Number.isInteger(calc.num)
+        ? calc.num.toString() + "."
+        : calc.num,
+        res: calc.res
+      })
+    }
+
+    const fractionClick = (value) => {
+      setFraction({
+        num: eval(fraction.num)+ eval(value)
+
+      })
+    }
+
+    const ftAndInClick = () => {
+      setCalc({num: calc.num, isFt: !calc.isFt, res: calc.res})
+    }
+
+  const numAdd = (value) => {
+
+
+     if ( calc.isFt) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+          ? "0" 
+          : calc.num === 0 
+          ? calc.num = value
+          : calc.num.toString() + value,
+        res: !calc.sign ? 0 : calc.res,
+      });
+
+     }
+     else {
+      setCalc({
+        
+      })
+     }
+      
+  }
   return (
     <Wrapper>
-      <Screen value={calc.num ? calc.num : calc.res} />
+  
+      <Screen value={calc.num ? calc.num + " ' " : calc.res + " ' "} 
+      value2={fraction.num + '"'} isFt = {calc.isFt}
+      />
+      
       <ButtonBox>
-        {
-          btnValues.flat().map((btn, i) => {
+        {btnValues.flat().map((btn, i) => {
             return (
               <Button
                 key={i}
                 className={btn === "=" ? "equals" : ""}
                 value={btn}
-                onClick={() => {
+                onClick = {(e) => {
                   if (btn === "C") {
-                    setCalc({sign: "+", num: 0, res: 0});
+                    clearCalc()
                   }
                   else if (btn === "+-") {
-                    if(calc.sign === "+") {
-                      setCalc({sign: "-"})
-                    }
-                    else {
-                      setCalc({sign: "+"})
-                    }
+                    changeSign()
                   }
-                  else if (btn === "%") {
 
+                  else if (btn === "%") {
+                    percentClick()
                   }
-                  else if (btn === "/") {
-                    
+                  else if (btn === "/" || btn === "X" || btn === "+" || btn === "-") {
+                    signClick(btn)
+                  }
+                  else if (btn === "ft/in") {
+                    ftAndInClick()
+                  }
+                  else if (btn === "."){
+                    decimalClick()
+                  }
+                  else if (btn === "="){
+                    equalsClick()
+                  }
+                  else if (btn === "1/2" || btn === "1/4" || btn === "1/8" || btn === "1/16" || btn === "1/32") {
+                    fractionClick(btn)
+                  }
+
+                  else {
+                    numAdd(btn)
                   }
                   
-                }}
+                }
+              }
               />
             );
-          })
-        }
+          })}
       </ButtonBox>
     </Wrapper>
   );
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,12 +1,32 @@
 import { Textfit } from "react-textfit";
 import "./Screen.css";
 
-const Screen = ({ value }) => {
-  return (
-    <Textfit className="screen" mode="single" max={70}>
-      {value}
+const Screen = ({ value, value2, isFt}) => {
+  if (isFt === true) {
+    return (
+    <>
+    <Textfit className="selected" mode="single" max={70}>
+        {value} 
     </Textfit>
+    <Textfit className="screen" mode="single" max={70}>
+       {value2} 
+    </Textfit>
+    </>
   );
+}
+  else if (isFt === false) {
+    return (
+     <>
+    <Textfit className="screen" mode="single" max={70}>
+        {value} 
+    </Textfit>
+    <Textfit className="selected" mode="single" max={70}>
+       {value2} 
+    </Textfit>
+    </>
+    );
+  }
+  
 };
 
 export default Screen;
